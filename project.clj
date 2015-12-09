69fb420342ee7d147088975ed2a3090efc73918f
Bogdan Apetrei <bogdanapetrei85@gmail.com>
2015-12-09 23:16:52 +0200

    update clojure library to 1.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject learnclojure "1.0.0-SNAPSHOT"
   :description "Learn Clojure"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :main learnclojure.core)
